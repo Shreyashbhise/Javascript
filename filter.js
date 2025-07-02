354eccb5a2e9f6ce2e9038b1f9f6bad152f45440
Shreyash Bhise <108046802+Shreyashbhise@users.noreply.github.com>
2025-07-02 23:17:24 +0530

    Create filter.js

---
+++ filter.js
@@ -0,0 +1,11 @@
+🔹 filter() – Select specific elements
+Creates a new array with only the elements that pass a condition.
+
+✅ Example:
+js
+Copy
+Edit
+const numbers = [1, 2, 3, 4];
+const evens = numbers.filter(num => num % 2 === 0);
+
+console.log(evens); // [2, 4]
