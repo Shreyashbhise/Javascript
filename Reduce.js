99833cc3c0a8929057aaf87b118fdb93d0342cfb
Shreyash Bhise <108046802+Shreyashbhise@users.noreply.github.com>
2025-07-02 23:18:02 +0530

    Create Reduce.js

---
+++ Reduce.js
@@ -0,0 +1,11 @@
+🔹 reduce() – Combine all elements into one value
+Reduces the array to a single value by applying a function.
+
+✅ Example:
+js
+Copy
+Edit
+const numbers = [1, 2, 3, 4];
+const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+
+console.log(sum); // 10
