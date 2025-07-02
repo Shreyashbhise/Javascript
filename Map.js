abfaefe526c1fc2b4c3d8a5643006aecb27be94b
Shreyash Bhise <108046802+Shreyashbhise@users.noreply.github.com>
2025-07-02 23:16:55 +0530

    Create Map.js

---
+++ Map.js
@@ -0,0 +1,9 @@
+🔹 map() – Transform each element
+Creates a new array by applying a function to every element.
+
+✅ Example:
+
+const numbers = [1, 2, 3, 4];
+const doubled = numbers.map(num => num * 2);
+
+console.log(doubled); // [2, 4, 6, 8]
