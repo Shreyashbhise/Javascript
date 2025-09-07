6fceb143e3dc9d626b2ec10c5d69e2174c8e4577
Shreyash Bhise <108046802+Shreyashbhise@users.noreply.github.com>
2025-09-07 10:06:15 +0530

    Create Filter.js

---
+++ Filter.js
@@ -0,0 +1,11 @@
+filter()
+
+👉 Creates a new array with elements that pass a condition.
+
+Length may be shorter.
+
+Used for filtering values.
+
+const nums = [1, 2, 3, 4, 5];
+const evens = nums.filter(n => n % 2 === 0);
+console.log(evens); // [2, 4]
